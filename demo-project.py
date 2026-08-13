cb638620527894b3532a1e57099c9c49bfebcd0a
palak18gabhane <palak18gabhane@gmail.com>
2026-08-13 10:01:26 +0530

    Created student management system

--- demo-project.py
+++ demo-project.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+students = []
+
+def add_student():
+    name = input("Enter student name: ")
+    roll_no = input("Enter roll number: ")
+    branch = input("Enter branch: ")
+
+    student = {
+        "name": name,
+        "roll_no": roll_no,
+        "branch": branch
+    }
+
+    students.append(student)
+    print("Student added successfully!")
